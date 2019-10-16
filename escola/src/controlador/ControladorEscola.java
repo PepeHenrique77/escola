cd413c0e6735ecd07b6427f3d91aeabc6f07e875
Administrador <Administrador@10.10.4.18>
2019-10-16 14:41:39 -0300

    prova

---
+++ escola/src/controlador/ControladorEscola.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import tela.manutencao.ManutencaoEscola;
+import dao.DaoEscola;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+import tela.manutencao.ManutencaoEscola;
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorEscola {
+
+    public static void inserir(ManutencaoEscola man){
+        Escola objeto = new Escola();
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setNr_alunos(Integer.parseInt(man.jtfNrDeAlunos.getText()));
+        objeto.setArea(Double.parseDouble(man.jtfArea.getText()));
+        objeto.setNr_funcionarios(Integer.parseInt(man.jtfNrDeFuncionarios.getText()));
+        objeto.setAbertura(LocalDate.parse(man.jtfAbertura.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        boolean resultado = DaoEscola.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+            if (man.listagem != null) {
+     atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+}
+man.dispose();//fechar a tela da manutenção
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+    public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Nr_alunos");
+        modelo.addColumn("Area");
+        modelo.addColumn("Nr_funcionarios");
+        modelo.addColumn("Abertura");
+        List<Escola> resultados = DaoEscola.consultar();
+        for (Escola objeto : resultados) {
+            Vector linha = new Vector();
+            
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNome());
+            linha.add(objeto.getNr_alunos());
+            linha.add(objeto.getArea());
+            linha.add(objeto.getNr_funcionarios());
+            linha.add(objeto.getAbertura());
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+    }
+
+    public static void alterar(ManutencaoEscola man){
+        Escola objeto = new Escola();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setNr_alunos(Integer.parseInt(man.jtfNrDeAlunos.getText()));
+        objeto.setArea(Double.parseDouble(man.jtfArea.getText()));
+        objeto.setNr_funcionarios(Integer.parseInt(man.jtfNrDeFuncionarios.getText()));
+        objeto.setAbertura(LocalDate.parse(man.jtfAbertura.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        boolean resultado = DaoEscola.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+            if (man.listagem != null) {
+     atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+}
+man.dispose();//fechar a tela da manutenção
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+    public static void excluir(ManutencaoEscola man){
+        Escola objeto = new Escola();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
+        
+        boolean resultado = DaoEscola.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+            if (man.listagem != null) {
+     atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+}
+man.dispose();//fechar a tela da manutenção
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+    public static void atualizaCampos(ManutencaoEscola man, int pk){ 
+        Escola objeto = DaoEscola.consultar(pk);
+        //Definindo os valores do campo na tela (um para cada atributo/campo)
+        man.jtfCodigo.setText(objeto.getCodigo().toString());
+        man.jtfNome.setText(objeto.getNome());
+        man.jtfNrDeAlunos.setText(objeto.getNr_alunos().toString());
+        man.jtfArea.setText(objeto.getArea().toString());
+        man.jtfNrDeFuncionarios.setText(objeto.getNr_funcionarios().toString());
+        man.jtfAbertura.setText(objeto.getAbertura().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        
+        man.jtfCodigo.setEnabled(false); //desabilitando o campo código
+        man.btnAdicionar.setEnabled(false); //desabilitando o botão adicionar
+    }
+
+}
